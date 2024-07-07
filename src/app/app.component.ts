05a313fd20dc8f3e6777d1369cf8363cff4b4506
darragh1991 <darraghwoods@live.ie>
2024-07-07 19:15:13 +0100

    adding jest, removing karma and testing serve conntention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'international-sports-club';
+
+  data = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.data.get('http://localhost:3000/users').subscribe((data) => console.log(data));
+
+  }
 }
