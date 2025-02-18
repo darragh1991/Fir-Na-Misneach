b4581374e7316aa4118cbddabfa89cae037e06a2
darragh1991 <darraghwoods@live.ie>
2025-02-18 18:37:41 +0000

    add filteration on question in FAQs

--- src/app/feature/faq/services/faq.service.ts
+++ src/app/feature/faq/services/faq.service.ts
@@ -1,14 +1,39 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
+import { inject, Injectable, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { Observable, tap } from 'rxjs';
+export interface Faq {
+  id: number;
+  question: string;
+  answer: string;
+}
 @Injectable()
 export class FaqService {
 
   httpClient = inject(HttpClient);
+  private faqsSignal = signal<Faq[]>([]);
+  data: Faq[] = [];
 
-  getFaqs(): Observable<any> {
-    return this.httpClient.get('/faqs');
+  loadFaqs() {
+    return this.httpClient.get<{ data: Faq[] }>('/faqs').pipe(
+      tap(({ data }) => {
+        this.data = data;
+        this.faqsSignal.set(data)
+  }));
+  }
+  resetFaqs(): void {
+    this.faqsSignal.set(this.data);
+  }
+  filterByFaq(filter: string): void {
+    this.resetFaqs();
+    this.faqsSignal.update((faqs) => {
+      return faqs.filter(faq => {
+        return faq.question.toLowerCase().includes(filter.toLowerCase());
+      });
+    });
   }
 
+  getFaqs$(): Observable<Faq[]> {
+    return toObservable(this.faqsSignal);
+  }
 }
