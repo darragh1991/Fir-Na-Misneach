90fd9c36f35da6a2bf504088254b006d9bb59d26
darragh1991 <darraghwoods@live.ie>
2025-03-22 18:37:38 +0000

    remove commented code

--- server/tests/controllers/faqs.controller.spec.js
+++ server/tests/controllers/faqs.controller.spec.js
@@ -32,26 +32,4 @@ describe('FaqController', () => {
         data: mockFaqs
       });
   });
-
-  // it('should send a success response for getOne', async () => {
-  //   const mockFaq = { id: 1, question: 'What is this?', answer: 'This is a FAQ.' };
-  //   faqsService.getById.mockReturnValue(mockFaq);
-
-  //   const response = await request(app).get('/faqs/1');
-  //   expect(response.status).toBe(200);
-  //   expect(response.body).toEqual({
-  //     data: mockFaq
-  //   });
-  // });
-
-  // it('should send an error response for getOne when FAQ not found', async () => {
-  //   faqsService.getById.mockReturnValue(undefined);
-
-  //   const response = await request(app).get('/faqs/999');
-  //   expect(response.status).toBe(404);
-  //   expect(response.body).toEqual({
-  //     message: 'No faq found',
-  //     data: undefined
-  //   });
-  // });
 });
